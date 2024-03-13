4be839924f1a1bcaee754da9404943acdaad708d
sagarmi4545 <58340060+sagarmi4545@users.noreply.github.com>
2024-03-13 09:06:21 +0530

    added function responses

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -32,15 +32,34 @@ def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to django session.
     '''
-    return True, ""
+    correct_answer = PYTHON_QUESTION_LIST[current_question_id]["answer"]
+    selected_answer = answer
+    if correct_answer == selected_answer:
+        score = 1
+        error = None
+    else:
+        score = 0
+        error = "You have selected wrong aswer! Please try again"
+    cumulated_score = session.get("TotalScore", None)
+    if cumulated_score is None:
+        session["TotalScore"] = score
+    else:
+        session["TotalScore"] += score
+    
+    return bool(score), error
 
 
 def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
-
-    return "dummy question", -1
+    total_questions_count = len(PYTHON_QUESTION_LIST)
+    next_question_id = current_question_id + 1
+    if next_question_id <= total_questions_count -1:
+        next_question = PYTHON_QUESTION_LIST[next_question_id]
+        return next_question, next_question_id
+    else:
+        return None, None
 
 
 def generate_final_response(session):
@@ -48,5 +67,10 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
-
+    total_score = session.get("TotalScore", 0)
+    total_questions_count = len(PYTHON_QUESTION_LIST)
+    if total_score < (0.4*total_questions_count):
+        summary = f"Good Effort! You score is {total_score}. Better Luck Next Time!"
+    else:
+        summary = f"Excellent! Your score is {total_score}. Keep it up!"
     return "dummy result"
